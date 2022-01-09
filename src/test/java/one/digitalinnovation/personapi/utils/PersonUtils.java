dacfe9f501fa8b019804baa6613d56c1ce22ef09
calljef <jeferson.nascimento@yahoo.com.br>
2022-01-09 10:16:20 -0300

    update class person utils

--- src/test/java/one/digitalinnovation/personapi/utils/PersonUtils.java
+++ src/test/java/one/digitalinnovation/personapi/utils/PersonUtils.java
@@ -10,7 +10,7 @@ public class PersonUtils {
 
     private static final String FIRST_NAME = "Jeferson";
     private static final String LAST_NAME = "Nascimento";
-    private static final String CPF_NUMBER = "369.333.878-79";
+    private static final String CPF_NUMBER = "073.338.077-89";
     private static final long PERSON_ID = 1L;
     public static final LocalDate BIRTH_DATE = LocalDate.of(1976, 1, 19);
 
@@ -19,7 +19,7 @@ public class PersonUtils {
                 .firstName(FIRST_NAME)
                 .lastName(LAST_NAME)
                 .cpf(CPF_NUMBER)
-                .birthDate("19-02-1976")
+                .birthDate("19-01-1976")
                 .phones(Collections.singletonList(PhoneUtils.createFakeDTO()))
                 .build();
     }
